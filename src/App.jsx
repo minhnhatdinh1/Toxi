c4e4bf8f6056e8cd376fc81ee4e38d4f030f5463
minhnhatdinh1 <minhnhatdinh78@gmail.com>
2026-04-13 21:59:45 +0700

    fix vercel spa routing

--- src/App.jsx
+++ src/App.jsx
@@ -95,6 +95,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<Navigate to="/home" replace />} />
         <Route path="home" element={<HomePage />} />
+        <Route path="Home" element={<Navigate to="/home" replace />} />
 
         <Route path="/blog" element={<Blog />} />
         
